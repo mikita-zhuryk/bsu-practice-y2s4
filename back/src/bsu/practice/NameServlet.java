0e6282b1b1fb035ce79aabfe9789dd6aba75e696
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-04-22 19:25:47 +0300

    Added get servlet

---
+++ back/src/bsu/practice/NameServlet.java
@@ -0,0 +1,34 @@
+package bsu.practice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="name", urlPatterns = "/get")
+public class NameServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder responseHTML = new StringBuilder("<html><body>");
+        String query = req.getQueryString();
+        String[] queryParams = query.split("&");
+        String paramName;
+        String paramValue;
+        String[] tuple;
+        for (String param: queryParams) {
+            tuple = param.split("=");
+            paramName = tuple[0];
+            paramValue = tuple[1];
+            responseHTML.append("<p>");
+            responseHTML.append(paramName);
+            responseHTML.append(" = ");
+            responseHTML.append(paramValue);
+            responseHTML.append("</p>");
+        }
+        responseHTML.append("</body></html>");
+        resp.getOutputStream().print(responseHTML.toString());
+    }
+}
