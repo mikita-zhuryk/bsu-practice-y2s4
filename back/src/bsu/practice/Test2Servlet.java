89bb2c2e0dc0491fcea4c54da6ca4641ae65c0b0
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2019-04-22 19:16:25 +0300

    Added redirect test servlet

---
+++ back/src/bsu/practice/Test2Servlet.java
@@ -0,0 +1,17 @@
+package bsu.practice;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="test2", urlPatterns = "/test2")
+public class Test2Servlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("page");
+    }
+}
